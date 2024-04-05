064ced834fdf7b4f3e885ae4459fad43376d341f
flowuidesign <flowuidesign@gmail.com>
2024-04-06 01:07:03 +0200

    commit

--- frontend/adapt_app/lib/main.dart
+++ frontend/adapt_app/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:adapt_app/screens/landing_screen.dart';
-//import 'package:package:adapt_app/screens/forgot_password.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,7 +17,6 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      //home: const ForgotPassword(),
       home: const LandingScreen(),
     );
   }
